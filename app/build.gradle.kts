d8cb34507802dc188219ae509c2511d33819efaa
Matvey <romankovmatvej59@gmail.com>
2025-11-30 21:02:18 +0700

    созданы активности входа и альбомов

---
+++ app/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.pz9_romankov_pr_24106"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.pz9_romankov_pr_24106"
+        minSdk = 21
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+}
